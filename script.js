b8a80fbef259e270daa976bcca0ba87da30aa514
ashferoz <ash.safrina@gmail.com>
2024-05-28 11:32:40 +0800

    create timer function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+// timer function (to change timer duration => change value of duration variable)
+let timerInSeconds = setInterval(updateTimer, 1000);
+
+let duration = 10;
+
+const timerElement = document.querySelector("#timer");
+
+function updateTimer() {
+  const minutes = Math.floor(duration / 60);
+  let seconds = duration % 60;
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  } else {
+    seconds = seconds;
+  }
+
+  timerElement.innerText = minutes + ":" + seconds;
+  duration--;
+
+  if (duration < 0) {
+    duration = 0;
+    clearInterval(timerInSeconds);
+  }
+}
